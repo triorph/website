8ecb5236a9af193ebbf666be3e0e210564ead09b
Michael Walsh <Michael.Walsh@canterbury.ac.nz>
2021-11-21 12:17:32 +1300

    Change the deploy script to autobuild and autoinvalidate

--- frontend/deploy_to_aws.sh
+++ frontend/deploy_to_aws.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 export AWS_PROFILE="miek"
+ng build
 cd dist/personal-website/
 aws s3 sync --delete . s3://miek-personal-website/
+aws cloudfront create-invalidation --distribution-id E1YA4S1E16V0WB --paths "/*"
